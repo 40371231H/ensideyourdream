fb3823e51525a9e789a469f017f0555f18eb76be
40371231H <40371231H@live.ntnu.edu.tw>
2017-10-19 10:38:28 +0800

    Pretty file

--- major/major.ino
+++ major/major.ino
@@ -9,42 +9,46 @@
 const int SLAVE_ADDRESS = 1;
 char incomingByte = 0;
 
-void setup() {  
-  Wire.begin();         // join I2C bus as a Master
-  
+void setup()
+{
+  Wire.begin(); // join I2C bus as a Master
+
   Serial.begin(9600);
   Serial.println("Type something to send:");
-  
+
   pinMode(redled, OUTPUT);
   pinMode(greenled, OUTPUT);
   pinMode(blueled, OUTPUT);
 }
 
-void loop() {
-  switch(incomingByte) {
-       case '6':
-            analogWrite(redled, 255);
-            analogWrite(greenled, 0);
-            analogWrite(blueled, 0);
-          break;
-        case '7':
-          analogWrite(redled, 0);
-            analogWrite(greenled, 255);
-            analogWrite(blueled, 0);
-          break;
-        case '8':
-          analogWrite(redled, 0);
-            analogWrite(greenled, 0);
-            analogWrite(blueled, 255);
-          break;
-        case '9':
-        break;
-        default:
-        break;
-    }
+void loop()
+{
+  switch (incomingByte)
+  {
+  case '6':
+    analogWrite(redled, 255);
+    analogWrite(greenled, 0);
+    analogWrite(blueled, 0);
+    break;
+  case '7':
+    analogWrite(redled, 0);
+    analogWrite(greenled, 255);
+    analogWrite(blueled, 0);
+    break;
+  case '8':
+    analogWrite(redled, 0);
+    analogWrite(greenled, 0);
+    analogWrite(blueled, 255);
+    break;
+  case '9':
+    break;
+  default:
+    break;
+  }
 }
 
-void serialEvent() {
+void serialEvent()
+{
   // read one byte from serial port
   incomingByte = Serial.read();
 
--- minor/minor.ino
+++ minor/minor.ino
@@ -16,123 +16,137 @@ void dice(int);
 
 int pos = 1;
 
-void setup() {
+void setup()
+{
   //I2C定義接收
   Wire.begin(SLAVE_ADDRESS);    // join I2C bus as a slave with address 1
   Wire.onReceive(receiveEvent); // register event
-  
+
   Serial.begin(9600);
   Serial.println("Received data:");
-  
+
   //定義腳位輸出
-  for(int i = 2;i <= 9;i++) {
-     pinMode(i, OUTPUT); 
+  for (int i = 2; i <= 9; i++)
+  {
+    pinMode(i, OUTPUT);
   }
   ledRun();
   light(pos);
 }
 
-void loop() {
-  switch(incomingByte) {
-    case '0':
-      dice(1);
-      break;
-    case '1':
-      dice(2);
-      break;
-    case '2':
-      dice(3);
-      break;
-    case '3':
-      dice(4);
-      break;
-    case '4':
-      dice(5);
-      break;
-    case '5':
+void loop()
+{
+  switch (incomingByte)
+  {
+  case '0':
+    dice(1);
+    break;
+  case '1':
+    dice(2);
+    break;
+  case '2':
+    dice(3);
+    break;
+  case '3':
+    dice(4);
+    break;
+  case '4':
+    dice(5);
+    break;
+  case '5':
     dice(6);
-      break;
-    default:
-      break;
+    break;
+  default:
+    break;
   }
 }
 
-void wait(long interval) {
-  
+void wait(long interval)
+{
 }
 
-void light(int led) { 
-  switch (led) {
+void light(int led)
+{
+  switch (led)
+  {
   case 1:
-      digitalWrite(2, HIGH);
-      break;
-    case 2:
-      digitalWrite(3, HIGH);
-      break;
-    case 3:
-      digitalWrite(4, HIGH);
-      break;
-    case 4:
-      digitalWrite(5, HIGH);
-      break;
-    case 5:
-      digitalWrite(6, HIGH);
-      break;
-    case 6:
-      digitalWrite(7, HIGH);
-      break;
-    case 7:
-      digitalWrite(8, HIGH);
-      break;
-    case 8:
-      digitalWrite(9, HIGH);
-      break;
+    digitalWrite(2, HIGH);
+    break;
+  case 2:
+    digitalWrite(3, HIGH);
+    break;
+  case 3:
+    digitalWrite(4, HIGH);
+    break;
+  case 4:
+    digitalWrite(5, HIGH);
+    break;
+  case 5:
+    digitalWrite(6, HIGH);
+    break;
+  case 6:
+    digitalWrite(7, HIGH);
+    break;
+  case 7:
+    digitalWrite(8, HIGH);
+    break;
+  case 8:
+    digitalWrite(9, HIGH);
+    break;
   }
 }
 
-void lightAll() {
-  for(int i = 1;i <= 8;i++) {
+void lightAll()
+{
+  for (int i = 1; i <= 8; i++)
+  {
     light(i);
   }
 }
 
-void dark(int led){ 
-  switch (led) {
+void dark(int led)
+{
+  switch (led)
+  {
   case 1:
-      digitalWrite(2, LOW);
-      break;
-    case 2:
-      digitalWrite(3, LOW);
-      break;
-    case 3:
-      digitalWrite(4, LOW);
-      break;
-    case 4:
-      digitalWrite(5, LOW);
-      break;
-    case 5:
-      digitalWrite(6, LOW);
-      break;
-    case 6:
-      digitalWrite(7, LOW);
-      break;
-    case 7:
-      digitalWrite(8, LOW);
-      break;
-    case 8:
-      digitalWrite(9, LOW);
-      break;
+    digitalWrite(2, LOW);
+    break;
+  case 2:
+    digitalWrite(3, LOW);
+    break;
+  case 3:
+    digitalWrite(4, LOW);
+    break;
+  case 4:
+    digitalWrite(5, LOW);
+    break;
+  case 5:
+    digitalWrite(6, LOW);
+    break;
+  case 6:
+    digitalWrite(7, LOW);
+    break;
+  case 7:
+    digitalWrite(8, LOW);
+    break;
+  case 8:
+    digitalWrite(9, LOW);
+    break;
   }
 }
 
-void darkAll() {
-  for(int i = 1;i <= 8;i++) {
+void darkAll()
+{
+  for (int i = 1; i <= 8; i++)
+  {
     dark(i);
   }
 }
 
-void ledRun() {
-  for(int i = 1;i <= 8;i++) {
+void ledRun()
+{
+  for (int i = 1; i <= 8; i++)
+  {
     light(i);
     delay(100);
     dark(i);
@@ -140,22 +154,27 @@ void ledRun() {
   }
 }
 
-void blinkLED(int pos, int num) {
+void blinkLED(int pos, int num)
+{
 }
 
-void dice(int num) {
+void dice(int num)
+{
   int MAXpos = pos + num;
-  for(int i = 1;i <= num;i++) {
+  for (int i = 1; i <= num; i++)
+  {
     pos++;
     light(pos);
-    if(pos == MAXpos)
+    if (pos == MAXpos)
       break;
     delay(100);
   }
 }
 
-void receiveEvent(int howMany) {
-  while (Wire.available()) {
+void receiveEvent(int howMany)
+{
+  while (Wire.available())
+  {
     // receive one byte from Master
     incomingByte = Wire.read();
     Serial.print(incomingByte);
